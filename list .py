dfc77073e5f974992447b41714f1a45a7e6447af
jum648 <78852579+jum648@users.noreply.github.com>
2021-03-24 15:40:15 +0300

    Add files via upload

---
+++ list .py
@@ -0,0 +1,23 @@
+fruits=["apple","banana","cherry","orange","kiwi","manngo"]
+fruits[1]="blackcurrent"
+print(fruits)
+fruits[1:3]=["watermelon"]
+fruits.insert(2,"passion")
+fruits.append("orange")
+print(fruits)
+
+fruits=["orange","apple","mango"]
+fruits.pop(1)
+print(fruits)
+x=fruits.copy()
+print(x)
+x=fruits.index("mango")
+print(x)
+fruits.sort
+print(fruits)
+
+fruits=['apple','banana','cherry']
+for x in fruits:
+  print(x)
+for i in range(len(fruits)):
+  print(fruits[i])
